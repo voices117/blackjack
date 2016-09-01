f5a62ac8204ca6e01117df0303fb20ba50d36e31
Kleer LA <kleer@kleer-sandbox.(none)>
2016-09-01 15:35:59 -0300

    Extending Game implementation and tests.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,17 @@
+require_relative 'deck'
+
 class Game
 	attr_reader :score
 	
 	def initialize
-		@score = 0
+		@deck = Deck.new
+	end
+
+	def add_player player
+		@player = player
+	end
+
+	def deal
+		@player.add_card @deck.draw_card
 	end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,21 @@
 require_relative '../lib/game'
+require_relative '../lib/player'
 
 describe 'Game' do
-	it 'should start with a score of 0' do
+	it 'should be possible to add a player' do
 		g = Game.new
-		g.score
+		p = Player.new
+		g.add_player p
+	end
+
+	it 'should be possible to deal a card' do
+		g = Game.new
+		p = Player.new
+		g.add_player p
+		p.hand.size.should == 0
+		g.deal
+		p.hand.size.should == 1
+		g.deal
+		p.hand.size.should == 2
 	end
 end
